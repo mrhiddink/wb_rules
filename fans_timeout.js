72879870206639081eba2bf194bc299c6595e191
mrhiddink <118624415+mrhiddink@users.noreply.github.com>
2025-10-15 11:41:29 +0300

    Update fans_timeout.js
    Code optimization

--- fans_timeout.js
+++ fans_timeout.js
@@ -1,34 +1,14 @@
 //Правило для выключения вентилятора с задержкой после выключения света. Если выключен не весь свет, то вентилятор не выключится.
 function makeFanRule (light1,light2,light3,fan,set_timeout) {
   defineRule({
-    whenChanged: light1,
+    whenChanged: [light1, light2, light3],
     then:  function (newValue, devName, cellName) {
-        if ((dev[light2] == false) && (dev[light3] == false) && (dev[fan] == true)) {
+        if ((dev[light1] == false) && (dev[light2] == false) && (dev[light3] == false) && (dev[fan] == true)) {
           setTimeout(function() {
             dev[fan] = false;
           }, set_timeout);
         }
       }   
-  });
-  defineRule({
-    whenChanged: light2,
-    then:  function (newValue, devName, cellName) {
-        if ((dev[light1] == false) && (dev[light3] == false) && (dev[fan] == true)) { 
-          setTimeout(function() {
-            dev[fan] = false;
-          }, set_timeout);
-        }
-    }   
-  });
-  defineRule({
-    whenChanged: light3,
-    then:  function (newValue, devName, cellName) {
-        if ((dev[light1] == false) && (dev[light2] == false) && (dev[fan] == true)) { 
-          setTimeout(function() {
-            dev[fan] = false;
-          }, set_timeout);
-        }
-    }   
   });
 };
 //Создаем правила: (Светильник 1, Светильник 2, Светильник 3, Вентилятор, Тайм-аут выключения)
