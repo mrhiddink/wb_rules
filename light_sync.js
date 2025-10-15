b5febf7357b9ab239e9277eaea9e581e9ecf2d86
mrhiddink <118624415+mrhiddink@users.noreply.github.com>
2025-10-15 16:55:43 +0300

    Create light_sync.js

---
+++ light_sync.js
@@ -0,0 +1,13 @@
+//Синхронизируем светильники
+function makeLightSync(light1,light2) {
+  defineRule({ 
+    whenChanged: light1, //Ночник справа
+    then: function (newValue, devName, cellName) {
+                dev[light2] = dev[light1];
+    }
+  });
+};
+makeLightSync("wb-mr6c_204/K5", "wb-mrgbw-d-fw3_144/Channel 3 (G)"); //Ночник справа + подсветка стены
+makeLightSync("wb-mr6c_204/K6", "wb-mrgbw-d-fw3_61/Channel 4 (W)"); //Ночник слева + подсветка стены
+makeLightSync("wb-mrgbw-d-fw3_61/Channels 1_2 (B_R)", "wb-mrgbw-d-fw3_144/Channel 1 (B)"); //Мягкий свет в гостиной + картина
+makeLightSync("wb-mrgbw-d-fw3_61/Channels 1_2 (B_R)", "wb-mrgbw-d-fw3_144/Channel 2 (R)"); //Ночник свет в гостиной + вешалка
