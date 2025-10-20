399dd570c174e56f7c5e400646d66ad42f2dd3a6
mrhiddink <118624415+mrhiddink@users.noreply.github.com>
2025-10-20 16:35:11 +0300

    Update neptun_sync.js
    Added vacation mode

--- neptun_sync.js
+++ neptun_sync.js
@@ -1,3 +1,16 @@
+//Создаем вирутальное устройство для режима "В отпуске"
+defineVirtualDevice ("VacationMode", { //Создаем виртуальное устройство
+  title: "Режим отпуска", 
+	cells: {
+	On: {
+		type:"switch", //Выключатель устройства
+		title: "В отпуске",
+		order: 1,
+		value: false,
+	}
+    }
+});
+
 //Синхронизируем краны нептуна с проверкой, что включен режим двух зон
 function makeValveSync(Valve1,Valve2, dualzone) {
   defineRule({ 
@@ -12,16 +25,16 @@ function makeValveSync(Valve1,Valve2, dualzone) {
 };
 makeValveSync("neptun_240/Zona 1", "neptun_240/Zona 2", "neptun_240/Dual zone mode");
 
-//Исправляем самопроизвольноке закрыте кранов - открываем через 60 минут при отсутствии тревоги
+//Исправляем самопроизвольноке закрыте кранов - открываем через 15 минут при отсутствии тревоги и не в отпуске
 function makeValveOn(Valve1, Valve2, Alarm1, Alarm2) {
   defineRule({ 
     whenChanged: [Valve1, Valve2],
     then: function (newValue, devName, cellName) {
-      if ((dev[Alarm1] == false) && (dev[Alarm2] == false)) {
+      if ((dev[Alarm1] == false) && (dev[Alarm2] == false) && (dev[VacationMode/On] == false)) {
         setTimeout(function(){
         dev[Valve1] = true;
         dev[Valve2] = true;
-        }, 3600000);
+        }, 900000);
       }           
     }
   });
