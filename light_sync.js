142ad18b05caa8dc699120e3de02ae55c8babd1a
mrhiddink <118624415+mrhiddink@users.noreply.github.com>
2025-10-20 16:36:25 +0300

    Update light_sync.js

--- light_sync.js
+++ light_sync.js
@@ -1,4 +1,4 @@
-//Синхронизируем светильники
+//Синхронизируем светильники полностью
 function makeLightSync(light1,light2) {
   defineRule({ 
     whenChanged: light1,
@@ -7,7 +7,19 @@ function makeLightSync(light1,light2) {
     }
   });
 };
-makeLightSync("wb-mr6c_204/K5", "wb-mrgbw-d-fw3_144/Channel 3 (G)"); //Ночник справа + подсветка стены
-makeLightSync("wb-mr6c_204/K6", "wb-mrgbw-d-fw3_61/Channel 4 (W)"); //Ночник слева + подсветка стены
+//Синхронизируем светильники только на выключение (ведущий - light1)
+function makeLightOffSync(light1,light2) {
+  defineRule({ 
+    whenChanged: light1,
+    then: function (newValue, devName, cellName) {
+      if (dev[light1] == false) {
+          dev[light2] = false;
+      }
+    }
+  });
+};
+makeLightSync("wb-mr6c_204/K5", "wb-mrgbw-d-fw3_144/Channel 3 (G)"); //Ночник справа (у двери) + подсветка стены
+makeLightSync("wb-mr6c_204/K6", "wb-mrgbw-d-fw3_61/Channel 4 (W)"); //Ночник слева (у окна) + подсветка стены
 makeLightSync("wb-mrgbw-d-fw3_61/Channels 1_2 (B_R)", "wb-mrgbw-d-fw3_144/Channel 1 (B)"); //Мягкий свет в гостиной + картина
 makeLightSync("wb-mrgbw-d-fw3_61/Channels 1_2 (B_R)", "wb-mrgbw-d-fw3_144/Channel 2 (R)"); //Ночник свет в гостиной + вешалка
+makeLightOffSync("wb-mr6c_196/K4", "wb-mrgbw-d-fw3_121/Channel 1 (B)"); //Точечный свет в гостевом с/у + мягкий свет
