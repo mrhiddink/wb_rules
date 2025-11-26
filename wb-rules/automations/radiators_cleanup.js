0c1200d1cac347d9f5e676da55d45a77d7e49f3f
mrhiddink <118624415+mrhiddink@users.noreply.github.com>
2025-11-26 11:38:49 +0300

    Update radiators_cleanup.js

--- wb-rules/automations/radiators_cleanup.js
+++ wb-rules/automations/radiators_cleanup.js
@@ -12,16 +12,18 @@ function radiatorCleanup(radiator, control) {
     whenChanged: ['radiatorsCleanup/Cleanup'],
     then: function (newValue, devName, cellName) {
       log.info("Цикл очистки радиатора", radiator, "начат");
-      var state = dev[radiator + '/On']; //Запоминаем состояние радиатора
+      var stateR = dev[radiator + '/On']; //Запоминаем состояние радиатора
+      var stateC = dev[control]; //Запоминаем состояние привода
       dev[radiator + '/On'] = false; //Выключаем радиатор
-      setTiemout(function() {
-        dev[control] = true; 
-      }, 1000); //Ждем 1 секунду, открываем клапан
-      setTimeout(function() {
-        dev[control] = false;
-      }, 600000); //Ждем 15 минут, закрываем клапан
-      dev[radiator + '/On'] = state; //Возвращаем радиатор в первоначальное состояние
-      log.info("Цикл очистки радиатора", radiator, "завершен");
+      setTimeout(function() { //Ждем 1 секунду, открываем клапан
+        dev[control] = true;
+        setTimeout(function() { //Ждем 10 минут, закрываем клапан
+          dev[control] = false;
+          dev[radiator + '/On'] = stateR; //Возвращаем радиатор в первоначальное состояние
+          dev[control] = stateC;
+          log.info("Цикл очистки радиатора", radiator, "завершен");
+        }, 10*60*1000); 
+      }, 1000); 
       }  
     });
 }
